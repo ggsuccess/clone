20d87a015c7bbc3b5eac589b9aef3c722855bcda
ggsuccess <ggsuccess@naver.com>
2020-01-14 19:11:35 +0900

    create input

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import './formStyles.css';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+  renderError(meta) {
+    if (meta.touched && meta.error) {
+      return <div className="err__msg">{meta.error}</div>;
+    }
+  }
+  renderInputBox = ({ input, label, meta }) => {
+    return (
+      <>
+        <label>{label}</label>
+        <input {...input} />
+        {this.renderError(meta)}
+      </>
+    );
+  };
+  onSubmitForm(formValues) {
+    console.log(formValues);
+  }
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmitForm)}
+        className="ui__form"
+      >
+        <Field
+          name="title"
+          label="Enter Title"
+          component={this.renderInputBox}
+        />
+        <Field
+          name="description"
+          label="Enter Description"
+          component={this.renderInputBox}
+        />
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({ form: 'StreamCreate', validate })(StreamCreate);
