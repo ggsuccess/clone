2411da1f83373b7d3336216b7505891dc8440f99
ggsuccess <ggsuccess@naver.com>
2020-01-14 17:59:08 +0900

    redux

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import './GoogleAuth.css';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends Component {
-  state = {
-    isSignedIn: null
-  };
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client
@@ -15,13 +14,17 @@ class GoogleAuth extends Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
-  onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  onAuthChange = isSignedIn => {
+    if (isSignedIn) {
+      this.props.signIn(this.auth.currentUser.get().getId());
+    } else {
+      this.props.signOut();
+    }
   };
   onSignInClick = () => {
     this.auth.signIn();
@@ -30,9 +33,9 @@ class GoogleAuth extends Component {
     this.auth.signOut();
   };
   renderAuthButton() {
-    if (this.state.isSignedIn === null) {
+    if (this.props.isSignedIn === null) {
       return null;
-    } else if (this.state.isSignedIn) {
+    } else if (this.props.isSignedIn) {
       return (
         <button
           className="loginBtn loginBtn--google"
@@ -57,4 +60,8 @@ class GoogleAuth extends Component {
   }
 }
 
-export default GoogleAuth;
+const mapStateToProps = state => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
